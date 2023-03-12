83f3a6f21f6270bc049fcd774829c191682c64a5
Tharmsy! <101517800+Tharmsy@users.noreply.github.com>
2023-03-12 04:20:59 +0000

    Updated admin panel and made that text easier

--- home/adminpanel.php
+++ home/adminpanel.php
@@ -1,1 +1,60 @@
-
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Surgeme Admin Panel</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <nav>
+      <div class="nav-wrapper orange">
+        <a href="#" class="brand-logo"><img src="logo.png" alt="Surgeme Admin Panel"></a>
+        <ul id="nav-mobile" class="right">
+        </ul>
+      </div>
+    </nav>
+    <main>
+      <div class="container">
+        <div class="row">
+          <div class="col s12 m4">
+            <div class="card">
+              <div class="card-content">
+                <span class="card-title">Users</span>
+                <p>View and manage user profiles.</p>
+              </div>
+              <div class="card-action">
+                <a href="#">View<i class="material-icons right">arrow_forward</i></a>
+              </div>
+            </div>
+          </div>
+          <div class="col s12 m4">
+            <div class="card">
+              <div class="card-content">
+                <span class="card-title">Posts</span>
+                <p>View and manage user posts.</p>
+              </div>
+              <div class="card-action">
+                <a href="#">View<i class="material-icons right">arrow_forward</i></a>
+              </div>
+            </div>
+          </div>
+          <div class="col s12 m4">
+            <div class="card">
+              <div class="card-content">
+                <span class="card-title">Settings</span>
+                <p>Manage website settings.</p>
+              </div>
+              <div class="card-action">
+                <a href="#">View<i class="material-icons right">arrow_forward</i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  </body>
+</html>
