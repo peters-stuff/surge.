cb2b1f12e94a0487213091cf2ed9a68b9f5a9ebc
Tharmsy! <101517800+Tharmsy@users.noreply.github.com>
2023-03-12 04:23:54 +0000

    Forgot the css

--- home/adminpanel.php
+++ home/adminpanel.php
@@ -2,6 +2,108 @@
 <html>
   <head>
     <meta charset="UTF-8">
+    <style>
+        body {
+            font-family: 'Roboto', sans-serif;
+            background-color: #f5f5f5;
+          }
+          
+          nav {
+            height: 64px;
+            background-color: #f57c00;
+            border-radius: 0 0 10px 10px;
+          }
+          
+          nav .nav-wrapper {
+            padding-left: 20px;
+            border-radius: 0 0 10px 10px;
+          }
+          
+          nav a {
+            color: #fff;
+            font-size: 1rem;
+          }
+          
+          .brand-logo {
+            font-size: 1.5rem;
+            padding: 10px;
+          }
+          
+          .nav-wrapper > ul {
+            margin: 0;
+          }
+          
+          .nav-wrapper > ul > li {
+            display: inline-block;
+            margin-right: 20px;
+          }
+          
+          .nav-wrapper > ul > li:last-child {
+            margin-right: 0;
+          }
+          
+          .nav-wrapper > ul > li > a {
+            display: inline-block;
+            padding: 18px 20px;
+            border-radius: 4px;
+            background-color: #fff;
+            color: #f57c00;
+          }
+          
+          .nav-wrapper > ul > li > a:hover {
+            background-color: #fff;
+            color: #f57c00;
+          }
+          
+          
+          .brand-logo {
+            font-size: 1.5rem;
+            padding: 10px;
+          }
+          
+          .card {
+            background-color: #fff;
+            border-radius: 4px;
+            box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+            margin: 1rem;
+            transition: all 0.3s ease;
+          }
+          
+          .card:hover {
+            box-shadow: 0px 6px 12px rgba(0, 0, 0, 0.2);
+            transform: translateY(-5px);
+          }
+          
+          .card-title {
+            font-size: 1.5rem;
+            font-weight: 500;
+            margin-bottom: 1rem;
+          }
+          
+          .card-content p {
+            font-size: 1rem;
+            color: #555;
+            margin: 0;
+            line-height: 1.5;
+          }
+          
+          .card-action a {
+            color: #26a69a;
+            font-weight: 500;
+            transition: all 0.3s ease;
+          }
+          
+          .card-action a:hover {
+            color: #166b6d;
+            text-decoration: underline;
+          }
+          
+          .material-icons {
+            font-size: 1.5rem;
+            vertical-align: middle;
+          }
+          
+    </style>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Surgeme Admin Panel</title>
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700" rel="stylesheet">
