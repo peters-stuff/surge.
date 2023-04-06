4e0caa82927ad0e4f796cc8e98d8351eae2e7903
peters-stuff <88831905+peters-stuff@users.noreply.github.com>
2023-04-07 00:49:18 +0100

    Add files via upload

---
+++ scripts/dumppost.php
@@ -0,0 +1,42 @@
+<?php
+
+if(!@isset($_GET['u'])) {
+    echo "<html><head><script>window.location.replace('/newsurg/?e=el')</script></head></html>";
+    die();   
+}
+if(!@isset($_GET['ta'])) {
+    echo "<html><head><script>window.location.replace('/newsurg/?e=el')</script></head></html>";
+    die();   
+}
+if(@$_GET['u']=='') {
+echo "<html><head><script>window.location.replace('/newsurg/?e=el')</script></head></html>";
+    die();   
+}
+if(@$_GET['ta']=='') {
+    echo "<html><head><script>window.location.replace('/newsurg/?e=el')</script></head></html>";
+    die();   
+}
+@$a=@file_get_contents('C:/xampp/htdocs/newsurg/data/tempaccess/'.$_GET['u'].'.pass');
+if($_GET['ta']==$a) {}else{
+    echo "<html><head><script>window.location.replace('/newsurg/?e=el')</script></head></html>";
+    die();
+}
+
+?><?php
+if(isset($_GET['p'])){
+    $p=file_get_contents('C:/xampp/htdocs/newsurg/data/posts/posts/'.$_GET['p']);
+
+    if(!file_exists('C:/xampp/htdocs/newsurg/data/likes/'.$_GET['p'].'.likes')){
+        $l='';
+    }else{
+        $l=file_get_contents('C:/xampp/htdocs/newsurg/data/likes/'.$_GET['p'].'.likes');
+    }
+
+    echo $p.',,!!,,'.$l.',,!!,,'.$_GET['p'].',,!!,,';
+    if(strpos( $l, $_GET['u'] ) !== false) {
+        echo (count(explode(',,!!,,',$l))) . ',,!!,,true'. ',,!!,,'.$_GET['p'];} else {
+            echo (count(explode(',,!!,,',$l))) . ',,!!,,false,,!!,,'.$_GET['p'];   
+        }
+}
+
+?>
