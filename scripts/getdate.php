75967d995b2d735f881d54f4b45b924d5410ea26
peters-stuff <88831905+peters-stuff@users.noreply.github.com>
2023-04-14 01:48:28 +0100

    Delete scripts directory

--- scripts/getdate.php
+++
@@ -1,9 +0,0 @@
-<?php
-$u=$_GET['u'];
-if(file_exists('C:/xampp/htdocs/newsurg/data/user/date/'.$u.'.date')) {
-echo file_get_contents('C:/xampp/htdocs/newsurg/data/user/date/'.$u.'.date');
-
-} else {
-    echo " -- Error Requesting Date  -- ";
-}
-    ?>
--- scripts/isbanned2.php
+++
@@ -1,7 +0,0 @@
-<?php
-if(file_exists('C:/xampp/htdocs/newsurg/data/bans/'.$_GET['u'].'.ban')){
-    echo file_get_contents('C:/xampp/htdocs/newsurg/data/bans/'.$_GET['u'].'.ban').',,!!,,'.$_GET['p'];
-} else {
-    echo "no,,!!,,";
-}
-?>
